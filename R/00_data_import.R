a284872b808573d2f2eae34d55e369b2386cd476
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2016-12-22 19:00:46 -0800

    adding dec 22 flow cam data

---
+++ R/00_data_import.R
@@ -0,0 +1,39 @@
+### Flow cam data import
+### Dec 22 2016
+### Last updated by JB
+
+# load libraries ----------------------------------------------------------
+
+library(tidyverse)
+library(purrr)
+library(lubridate)
+library(stringr)
+
+# Step 1: get a list of all the files
+cell_files <- list.files("/Users/Joey/Documents/M.Tseng/flowcam-summaries-dec22", full.names = TRUE)
+
+
+names(cell_files) <- cell_files %>% 
+	gsub(pattern = ".csv$", replacement = "")
+
+
+#### Step 2: read in all the files!
+
+all_cells <- map_df(cell_files, read_csv, col_names = FALSE, .id = "file_name")
+
+
+#### Step 3: pull out just the data we want, do some renaming etc.
+
+scenedesmus_dec22 <- all_cells %>% 
+	rename(obs_type = X1,
+				 value = X2) %>% 
+	filter(obs_type %in% c("List File", "Start Time", "Particles / ml", "Volume (ABD)")) %>%
+	spread(obs_type, value) %>%
+	separate(`List File`, into = c("replicate", "other"), sep = "[:punct:]") %>% 
+	rename(start_time = `Start Time`,
+				 cell_density = `Particles / ml`,
+				 cell_volume = `Volume (ABD)`) %>% 
+	select(-other)
+
+#### Step 4: write out the summary data to file
+write_csv(scenedesmus_dec22, "data-processed/scenedesmus_dec22.csv")
