6a29346505bfd0e7b88699fd27055ca478cf5747
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2017-01-03 10:33:43 -0800

    adding plot script

---
+++ R/01_initial_plots.R
@@ -0,0 +1,37 @@
+### Cell count plots
+### Jan 03 2017
+### Last updated by JB
+
+# load libraries ----------------------------------------------------------
+
+library(tidyverse)
+library(purrr)
+library(lubridate)
+library(stringr)
+
+
+
+# read in data ------------------------------------------------------------
+
+cells <- read_csv("data-processed/scenedesmus_dec.csv")
+
+
+??lubridate
+# format dates ------------------------------------------------------------
+
+cells2 <- cells %>% 
+	mutate(date = day(start_time)) %>%
+	mutate(date = ifelse(date == "24", "December 24 2016", date)) %>% 
+	mutate(date = ifelse(date == "22", "December 22 2016", date)) %>% 
+	mutate(date = mdy(date))
+
+cells2 %>% 
+	mutate(replicate = factor(replicate)) %>% 
+	mutate(temperature = ifelse(as.numeric(replicate) < 8.5, "12C", "18C")) %>%
+	group_by(replicate) %>%
+ggplot(aes(x = date, y = cell_density, group = replicate, color = temperature)) + geom_point(size = 4) +
+	geom_line()
+
+
+	
+	
